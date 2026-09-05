19f6ba711141fef0b45c91acb87ca2652d33ad0e
Zach Baron <mixedbyinstinct@gmail.com>
2026-09-04 21:05:13 -0400

    unfreezing logo

--- frontend/src/components/MapHeader.jsx
+++ frontend/src/components/MapHeader.jsx
@@ -3,20 +3,25 @@ import buddiesLogo from "../assets/buddies-logo.svg";
 const MapHeader = () => {
   return (
     <header className="map-header">
-      <img
-        src={buddiesLogo}
-        alt="No Man's Sky Buddies"
-        className="map-header-logo"
-      />
+      <div className="map-header-content">
+        <img
+          src={buddiesLogo}
+          alt="No Man's Sky Buddies"
+          className="map-header-logo"
+        />
 
-      <h1 className="map-header-title">
-        WHERE ARE OUR BUDDIES
-      </h1>
-      <h1 className="map-header-sub-title">
-        UNDER CONSTRUCTION
-      </h1>
+        <div className="map-header-text">
+          <h1 className="map-header-title">
+            WHERE ARE OUR BUDDIES
+          </h1>
+
+          <h2 className="map-header-sub-title">
+            UNDER CONSTRUCTION
+          </h2>
+        </div>
+      </div>
     </header>
   );
-}
+};
 
 export default MapHeader;
